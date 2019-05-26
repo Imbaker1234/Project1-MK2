6487c0373448ffc8757ca7c7b3758fa462076c67
Ian Baker <Imbaker1234@gmail.com>
2019-05-26 13:58:29 -0400

    Pushing them POJOs

---
+++ 2. Service/src/main/java/com/POJO/ErsReimbursement.java
@@ -0,0 +1,107 @@
+package com.sample;
+
+
+public class ErsReimbursement {
+
+  private String reimbId;
+  private String reimbAmount;
+  private java.sql.Timestamp reimbSubmitted;
+  private java.sql.Timestamp reimbResolved;
+  private String reimbDescription;
+  private String reimbReceipt;
+  private String reimbAuthor;
+  private String reimbResolver;
+  private String reimbStatusId;
+  private String reimbTypeId;
+
+
+  public String getReimbId() {
+    return reimbId;
+  }
+
+  public void setReimbId(String reimbId) {
+    this.reimbId = reimbId;
+  }
+
+
+  public String getReimbAmount() {
+    return reimbAmount;
+  }
+
+  public void setReimbAmount(String reimbAmount) {
+    this.reimbAmount = reimbAmount;
+  }
+
+
+  public java.sql.Timestamp getReimbSubmitted() {
+    return reimbSubmitted;
+  }
+
+  public void setReimbSubmitted(java.sql.Timestamp reimbSubmitted) {
+    this.reimbSubmitted = reimbSubmitted;
+  }
+
+
+  public java.sql.Timestamp getReimbResolved() {
+    return reimbResolved;
+  }
+
+  public void setReimbResolved(java.sql.Timestamp reimbResolved) {
+    this.reimbResolved = reimbResolved;
+  }
+
+
+  public String getReimbDescription() {
+    return reimbDescription;
+  }
+
+  public void setReimbDescription(String reimbDescription) {
+    this.reimbDescription = reimbDescription;
+  }
+
+
+  public String getReimbReceipt() {
+    return reimbReceipt;
+  }
+
+  public void setReimbReceipt(String reimbReceipt) {
+    this.reimbReceipt = reimbReceipt;
+  }
+
+
+  public String getReimbAuthor() {
+    return reimbAuthor;
+  }
+
+  public void setReimbAuthor(String reimbAuthor) {
+    this.reimbAuthor = reimbAuthor;
+  }
+
+
+  public String getReimbResolver() {
+    return reimbResolver;
+  }
+
+  public void setReimbResolver(String reimbResolver) {
+    this.reimbResolver = reimbResolver;
+  }
+
+
+  public String getReimbStatusId() {
+    return reimbStatusId;
+  }
+
+  public void setReimbStatusId(String reimbStatusId) {
+    this.reimbStatusId = reimbStatusId;
+  }
+
+
+  public String getReimbTypeId() {
+    return reimbTypeId;
+  }
+
+  public void setReimbTypeId(String reimbTypeId) {
+    this.reimbTypeId = reimbTypeId;
+  }
+
+}
