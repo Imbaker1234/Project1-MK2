dfe41bc3267e493d21a0984da7bd06502089a362
Imbaker1234 <Imbaker1234@gmail.com>
2019-06-03 12:47:11 -0400

    Removed bootstrap stylings and implementing raw CSS for more manageability.

--- 2. Service/src/main/webapp/JS/script.js
+++ 2. Service/src/main/webapp/JS/script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     if (localStorage.jwt) {
-        console.log("window.onload() called : JWT Found: Loading Dashboard");
+        console.log(date.getTime() + " " + "window.onload() called : JWT Found: Loading Dashboard");
         loadDashboard();
     } else {
-        console.log("window.onload() called : JWT Not Found: Loading Login");
+        console.log(date.getTime() + " " + "window.onload() called : JWT Not Found: Loading Login");
         loadLogin();
     }
 };
@@ -11,7 +11,7 @@ window.onload = function () {
 //Functionalities =================================================
 
 function ajaxCall(method, endPoint, incoming, store) {
-    console.log("ajaxCall(method = " + method + "," + "endPoint = " + endPoint + ", incoming = " + incoming + ", store = " + store + ") called"); //DEBUG
+    console.log(date.getTime() + " " + "ajaxCall(method = " + method + "," + "endPoint = " + endPoint + ", incoming = " + incoming + ", store = " + store + ") called"); //DEBUG
     let outgoing = JSON.stringify(incoming);
 
     let xhr = new XMLHttpRequest();
@@ -25,18 +25,18 @@ function ajaxCall(method, endPoint, incoming, store) {
         if (xhr.readyState == 4 && xhr.status == 200) {
             let result = JSON.parse(xhr.responseText);
             if (result) {
-                console.log("Results retrieved from AJAX call"); //DEBUG
-                console.log(xhr.responseText); //DEBUG
+                console.log(date.getTime() + " " + "Results retrieved from AJAX call"); //DEBUG
+                console.log(date.getTime() + " " + xhr.responseText); //DEBUG
                 if (store) {
                     window.localStorage.setItem(store, xhr.responseText);
-                    console.log("Line 33");
+                    console.log(date.getTime() + " " + "Line 33");
                     if (xhr.responseText) {
-                        // console.log("=======Begin Response=======");
-                        // console.log(xhr.responseText); //DEBUG
-                        // console.log("=======Begin Header=======");
-                        // console.log(xhr.getAllResponseHeaders());
+                        // console.log(date.getTime() + " " + "=======Begin Response=======");
+                        // console.log(date.getTime() + " " + xhr.responseText); //DEBUG
+                        // console.log(date.getTime() + " " + "=======Begin Header=======");
+                        // console.log(date.getTime() + " " + xhr.getAllResponseHeaders());
                         if (xhr.getResponseHeader("Authorization")) localStorage.setItem("jwt", xhr.getResponseHeader("Authorization"));
-                        if (localStorage.getItem("jwt")) console.log("JWT STORED!\n\n" + localStorage.getItem("jwt")); //DEBUG
+                        if (localStorage.getItem("jwt")) console.log(date.getTime() + " " + "JWT STORED!\n\n" + localStorage.getItem("jwt")); //DEBUG
                         return xhr.responseText;
                     }
                 }
@@ -46,13 +46,13 @@ function ajaxCall(method, endPoint, incoming, store) {
 }
 
 function ajaxLoadDiv(view, targetDiv) {
-    console.log("ajaxLoadDiv(" + view + ", " + targetDiv + ") called");
+    console.log(date.getTime() + " " + date.getTime() + "ajaxLoadDiv(" + view + ", " + targetDiv + ") called");
     let xhr = new XMLHttpRequest();
     xhr.open("GET", view, true);
     xhr.send();
 
     xhr.onreadystatechange = function () {
-        console.log("Ready State " + xhr.readyState + " // Status " + xhr.status + ", " + xhr.statusText);
+        console.log(date.getTime() + " " + "Ready State " + xhr.readyState + " // Status " + xhr.status + ", " + xhr.statusText);
         if (xhr.readyState == 4 && xhr.status == 200) {
             document.getElementById(targetDiv).innerHTML = xhr.responseText;
         }
@@ -62,30 +62,30 @@ function ajaxLoadDiv(view, targetDiv) {
 //Load Views ======================================================
 
 function loadLogin() {
-    console.log("loadLogin() called"); //DEBUG
+    console.log(date.getTime() + " " + "loadLogin() called"); //DEBUG
     ajaxLoadDiv("login.view", "page");
 }
 
 function loadDashboard() {
-    console.log("loadDashboard() called"); //DEBUG
+    console.log(date.getTime() + " " + "loadDashboard() called"); //DEBUG
     ajaxLoadDiv("dashboard.view", "page");
     // let userToWelcome = localStorage.principal
     document.getElementById("dashboardwelcomeuser").innerText = "Hello Benis"
 }
 
 function loadTickets() {
-    console.log("loadTickets() called"); //DEBUG
+    console.log(date.getTime() + " " + "loadTickets() called"); //DEBUG
     ajaxLoadDiv("pasttickets.view", "ticketview");
     //this later to some sort of sub div on the dashboard.
 }
 
 function loadCurrentTickets() {
-	console.log("loadCurrentTickets() called"); //DEBUG
+    console.log(date.getTime() + " " + "loadCurrentTickets() called"); //DEBUG
 	ajaxLoadDiv("dashboard.view", "page");
 }
 
 function loadAllReimbs() {
-	console.log("loadAllReimbs() called"); //DEBUG
+    console.log(date.getTime() + " " + "loadAllReimbs() called"); //DEBUG
 	ajaxLoadDiv("admindashboard.view", "page");
 }
 
@@ -96,9 +96,9 @@ function login() {
     let username = document.getElementById("loginusername").value;
     let password = document.getElementById("loginpassword").value;
 
-    console.log("login() called"); //DEBUG
-    console.log("username = " + username); //DEBUG
-    console.log("password = " + password); //DEBUG
+    console.log(date.getTime() + " " + "login() called"); //DEBUG
+    console.log(date.getTime() + " " + "username = " + username); //DEBUG
+    console.log(date.getTime() + " " + "password = " + password); //DEBUG
 
     let credentials = [username, password];
     ajaxCall("POST", "account", credentials, "principal");
@@ -108,7 +108,7 @@ function login() {
 }
 
 function logout() {
-    console.log("logout() called"); //DEBUG
+    console.log(date.getTime() + " " + "logout() called"); //DEBUG
     localStorage.removeItem("jwt");
     LoadLogin();
 }
@@ -120,12 +120,12 @@ function register() {
     let firstname = document.getElementById("registerfirst").value;
     let lastname = document.getElementById("registerlast").value;
     let email = document.getElementById("registeremail").value;
-    console.log("verifyUsername() called"); //DEBUG
-    console.log("username =" + username); //DEBUG
-    console.log("password =" + password); //DEBUG
-    console.log("firstname =" + firstname); //DEBUG
-    console.log("lastname =" + lastname); //DEBUG
-    console.log("email =" + email); //DEBUG
+    console.log(date.getTime() + " " + "verifyUsername() called"); //DEBUG
+    console.log(date.getTime() + " " + "username =" + username); //DEBUG
+    console.log(date.getTime() + " " + "password =" + password); //DEBUG
+    console.log(date.getTime() + " " + "firstname =" + firstname); //DEBUG
+    console.log(date.getTime() + " " + "lastname =" + lastname); //DEBUG
+    console.log(date.getTime() + " " + "email =" + email); //DEBUG
 
     let credentials = [username, password, firstname, lastname, email];
 
@@ -134,11 +134,11 @@ function register() {
 
 function getTickets() {
 
-    console.log("getTickets() called"); //DEBUG
+    console.log(date.getTime() + " " + "getTickets() called"); //DEBUG
     let content = ["pasttickets"];
 
     let tickets = ajaxCall("POST", "dashboard", content);
-    console.log(tickets);
+    console.log(date.getTime() + " " + tickets);
 
     if (window.localStorage.getItem("jwt") != "") { //If they have a JWT, load the page
         loadTickets();
@@ -147,7 +147,7 @@ function getTickets() {
 
 function addReimbursement() {
 
-    console.log("addReimbursement() called"); //DEBUG
+    console.log(date.getTime() + " " + "addReimbursement() called"); //DEBUG
 	
 	let amt = document.getElementById("reimb_amount").value;
 	let desc = document.getElementById("reimb_desc").value;
@@ -155,7 +155,7 @@ function addReimbursement() {
 	let content = [amt, desc, type];
 	
 	ajaxCall("POST", "dashboard", content, "currenttickets");
-	console.log(window.localStorage.currenttickets);
+    console.log(date.getTime() + " " + window.localStorage.currenttickets);
 	
     if (window.localStorage.getItem("jwt") != "") { //If they have a JWT, load the page
         loadCurrentTickets();
@@ -164,12 +164,12 @@ function addReimbursement() {
 
 function viewAllReimbursements() {
 
-    console.log("viewAllReimbursements() called"); //DEBUG
+    console.log(date.getTime() + " " + "viewAllReimbursements() called"); //DEBUG
 	
 	let content = [viewallreimbs];
 	
 	ajaxCall("POST", "dashboard", content, "allreimbs");
-	console.log(window.localStorage.allreimbs);
+    console.log(date.getTime() + " " + window.localStorage.allreimbs);
 	
     if (window.localStorage.getItem("jwt") != "") { //If they have a JWT, load the page
         loadAllReimbs();
@@ -178,14 +178,14 @@ function viewAllReimbursements() {
 
 function updateReimbursementStatus() {
 
-    console.log("updateReimbursementStatus() called"); //DEBUG
+    console.log(date.getTime() + " " + "updateReimbursementStatus() called"); //DEBUG
 	
 	let newstatus = document.getElementById("reimb_amount").value;
 	let id = document.getElementById("reimb_id").value;
 	let content = [reimb_id, newstatus];
 	
 	ajaxCall("POST", "dashboard", content, "updatecheck");
-	console.log(window.localStorage.updatecheck);
+    console.log(date.getTime() + " " + window.localStorage.updatecheck);
 	
     if (window.localStorage.getItem("jwt") != "") { //If they have a JWT, load the page
     	loadAllReimbs();
@@ -194,13 +194,13 @@ function updateReimbursementStatus() {
 
 function filterAllReimbs() {
 
-    console.log("filterAllReimbs() called"); //DEBUG
+    console.log(date.getTime() + " " + "filterAllReimbs() called"); //DEBUG
 	
 	let filteredstatus = document.getElementById("filtered_status").value;
 	let content = [filtered_status];
 	
 	ajaxCall("POST", "dashboard", content, "filteredlist");
-	console.log(window.localStorage.filteredlist);
+    console.log(date.getTime() + " " + window.localStorage.filteredlist);
 	
     if (window.localStorage.getItem("jwt") != "") { //If they have a JWT, load the page
     	loadAllReimbs();
@@ -212,7 +212,7 @@ function filterAllReimbs() {
 
 
 function verifyField(field) {
-    console.log("verifyField(" + field + ") called"); //DEBUG
+    console.log(date.getTime() + " " + "verifyField(" + field + ") called"); //DEBUG
     return !(field == "" || field.includes(" ") || field.length < 3 || field.length > 24);
 }
 
@@ -232,7 +232,7 @@ function toggleButton(buttonId, status) {
 }
 
 function verifyName(firstname, lastname) {
-    console.log("verifyName(" + firstname + "," + lastname + ") called"); //DEBUG
+    console.log(date.getTime() + " " + "verifyName(" + firstname + "," + lastname + ") called"); //DEBUG
     if (firstname.length < 3 || lastname.length < 3) {
 
         document.getElementById("registerfirst").value = "";
@@ -253,7 +253,7 @@ function verifyName(firstname, lastname) {
 }
 
 function verifyPhone(phone) {
-    console.log("verifyPhone(" + phone + ") called"); //DEBUG
+    console.log(date.getTime() + " " + "verifyPhone(" + phone + ") called"); //DEBUG
     if ((/^\d{10}$/.test(phone))) {
         return true;
     }
@@ -261,7 +261,7 @@ function verifyPhone(phone) {
 }
 
 function verifyEmail(email) {
-    console.log("verifyEmail(" + email + ") called"); //DEBUG
+    console.log(date.getTime() + " " + "verifyEmail(" + email + ") called"); //DEBUG
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         return true;
     }
@@ -271,7 +271,7 @@ function verifyEmail(email) {
 }
 
 function verifyLoginFields() {
-    console.log("verifyLoginFields() called"); //DEBUG
+    console.log(date.getTime() + " " + "verifyLoginFields() called"); //DEBUG
     document.getElementById("registerdiv").classList.add("inactive");
     document.getElementById("registerusername").value = '';
     document.getElementById("registerpassword").value = '';
@@ -284,7 +284,7 @@ function verifyLoginFields() {
     let password = document.getElementById("loginpassword").value;
 
     if (verifyField(username) && verifyField(password)) {
-        console.log("Credentials valid. toggling button");
+        console.log(date.getTime() + " " + "Credentials valid. toggling button");
         toggleButton("loginsubmitbutton", true);
     } else {
         toggleButton("loginsubmitbutton", false);
@@ -292,7 +292,7 @@ function verifyLoginFields() {
 }
 
 function verifyRegisterFields() {
-    console.log("verifyRegisterFields() called");
+    console.log(date.getTime() + " " + "verifyRegisterFields() called");
 
     //Set the login div to inactive.
     document.getElementById("logindiv").classList.add("inactive");
@@ -313,7 +313,7 @@ function verifyRegisterFields() {
 
     //If all the forms are verified then active the "Start Getting Paid" button.
     if (verifyField(username) && verifyField(password) && verifyField(first) && verifyField(last) && verifyField(email)) {
-        console.log("Credentials valid. toggling button");
+        console.log(date.getTime() + " " + "Credentials valid. toggling button");
         toggleButton("registersubmitbutton", true);
 
         //If they are not valid then disable the register button.
@@ -323,7 +323,7 @@ function verifyRegisterFields() {
 }
 
 
-console.log("JS Loaded");
+console.log(date.getTime() + " " + "JS Loaded");
 
 
 //==================REFERENCE SCRIPT===============================
@@ -344,7 +344,7 @@ console.log("JS Loaded");
  }
 
  function updateNav(isAuth) {
- console.log('in updateNav()');
+ console.log(date.getTime() + " " + 'in updateNav()');
 
  if(isAuth) {
  $('#toLogin').attr('hidden', true);
@@ -365,14 +365,14 @@ console.log("JS Loaded");
 
  function isAuthenticated() {
  let authenticatedUser = window.localStorage.getItem('user');
- console.log(authenticatedUser);
+ console.log(date.getTime() + " " + authenticatedUser);
 
  if(authenticatedUser) return true
  else return false;
  }
 
  function loadLogin() {
-	 console.log('in loadLogin()');
+	 console.log(date.getTime() + " " + 'in loadLogin()');
 	
 	 let isAuth = isAuthenticated();
 	 updateNav(isAuth);
@@ -391,7 +391,7 @@ console.log("JS Loaded");
  }
 
  function loadRegister() {
- console.log('in loadRegister()');
+ console.log(date.getTime() + " " + 'in loadRegister()');
 
  let isAuth = isAuthenticated();
  updateNav(isAuth);
@@ -410,7 +410,7 @@ console.log("JS Loaded");
  }
 
  function loadHome() {
- console.log('in loadHome()');
+ console.log(date.getTime() + " " + 'in loadHome()');
 
  let isAuth = isAuthenticated();
  updateNav(isAuth);
@@ -429,7 +429,7 @@ console.log("JS Loaded");
  }
 
  function loadHomeInfo() {
- console.log('in loadHomeInfo()');
+ console.log(date.getTime() + " " + 'in loadHomeInfo()');
  let userJSON = window.localStorage.getItem('user');
  let user = JSON.parse(userJSON);
  $('#user_id').html(user.id);
@@ -441,7 +441,7 @@ console.log("JS Loaded");
  }
 
  function loadProfile() {
- console.log('in loadProfile()');
+ console.log(date.getTime() + " " + 'in loadProfile()');
 
  let isAuth = isAuthenticated();
  updateNav(isAuth);
@@ -452,7 +452,7 @@ console.log("JS Loaded");
  }
 
  function loadLoginInfo() {
- console.log('in loadLoginInfo()');
+ console.log(date.getTime() + " " + 'in loadLoginInfo()');
 
  $('#login-message').hide();
  $('#login').on('click', login);
@@ -460,7 +460,7 @@ console.log("JS Loaded");
  }
 
  function login() {
- console.log('in login()');
+ console.log(date.getTime() + " " + 'in login()');
 
  let username = $('#login-username').val();
  let password = $('#login-password').val();
@@ -480,7 +480,7 @@ console.log("JS Loaded");
  alert('Login successful!');
  window.localStorage.setItem('user', xhr.responseText);
  loadHome();
- console.log(`User id: ${user.id} login successful`);
+ console.log(date.getTime() + " " + `User id: ${user.id} login successful`);
  } else {
  $('#login-message').show();
  $('#login-message').html('Invalid credentials');
@@ -491,7 +491,7 @@ console.log("JS Loaded");
  }
 
  function logout() {
- console.log('in logout()');
+ console.log(date.getTime() + " " + 'in logout()');
  window.localStorage.removeItem('user');
 
  let xhr = new XMLHttpRequest();
@@ -500,14 +500,14 @@ console.log("JS Loaded");
 
  xhr.onreadystatechange = function() {
  if(xhr.readyState == 4 && xhr.status == 200) {
- console.log('Session has been invalidated!');
+ console.log(date.getTime() + " " + 'Session has been invalidated!');
  loadLogin();
  }
  }
  }
 
  function loadRegisterInfo() {
- console.log('in loadRegisterInfo()');
+ console.log(date.getTime() + " " + 'in loadRegisterInfo()');
 
  $('#reg-message').hide();
 
@@ -539,10 +539,10 @@ console.log("JS Loaded");
  }
 
  function validateUsername() {
- console.log('in validateUsername()');
+ console.log(date.getTime() + " " + 'in validateUsername()');
 
  let username = $('#reg-username').val();
- console.log(username);
+ console.log(date.getTime() + " " + username);
 
  if(username !== '') {
  let usernameJSON = JSON.stringify(username);
@@ -554,7 +554,7 @@ console.log("JS Loaded");
 
  xhr.onreadystatechange = function(){
  if(xhr.readyState == 4 && xhr.status == 200) {
- console.log(xhr.responseText);
+ console.log(date.getTime() + " " + xhr.responseText);
  let username = JSON.parse(xhr.responseText);
  if(!username) {
  $('#reg-message').show();
@@ -570,7 +570,7 @@ console.log("JS Loaded");
  }
 
  function validateEmail() {
- console.log('in validateEmail()');
+ console.log(date.getTime() + " " + 'in validateEmail()');
 
  let email = $('#email').val();
 
@@ -584,7 +584,7 @@ console.log("JS Loaded");
 
  xhr.onreadystatechange = function(){
  if(xhr.readyState == 4 && xhr.status == 200) {
- console.log(xhr.responseText);
+ console.log(date.getTime() + " " + xhr.responseText);
  let email = JSON.parse(xhr.responseText);
  if(!email) {
  $('#reg-message').show();
@@ -599,7 +599,7 @@ console.log("JS Loaded");
  }
 
  function register() {
- console.log('in register()');
+ console.log(date.getTime() + " " + 'in register()');
 
  $('#register').attr('disabled', true);
 
